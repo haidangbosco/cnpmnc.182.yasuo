4ed507a0d6bfd29ea3aa0a5ba0fb533f6f81a7ad
Duong Thai Minh <minh9a4@gmail.com>
2019-03-02 14:18:59 +0700

    fix missing author name

--- books.php
+++ books.php
@@ -1,14 +1,17 @@
 <?php 
-for($i=0;$i<10;++$i){
+$book = array(1, 2);
+ {
 ?>
     <div class="row border">
         <img class="image col-3" src="https://via.placeholder.com/150x150">
         <div class="col-9">
             <span class="h4">Name of The Book</span>
+            <span>Name of The Author</span>
+            <span class="bg-success">Available</span>
             <div class="row">
                 <p class="col-10"> Abstraction: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <div class="col-2">
-                    <div class="btn btn-success">Available</div>
+                    <button class="btn btn-default">Borrow</button>
                 </div>
             </div>
             <div class="row">
