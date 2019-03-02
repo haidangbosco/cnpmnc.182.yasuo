87b2cc51d74de18e0379e71e2a838baab686d9cc
haidangbosco <darkmatter2411@gmail.com>
2019-03-02 15:07:19 +0700

    add bookinfo.php

---
+++ bookinfo.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="row">
+        <div class="col-6">
+            <img src="download.jpeg" alt="No image found">
+        </div>
+        <div class="col-3"></div>
+        <div class="col-3"></div>
+    </div>
+    <div class="row"></div>
+    <div class="row"></div>
+</div>
