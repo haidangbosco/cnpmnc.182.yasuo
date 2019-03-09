ce1e2cb7b31945373ca65074fba2cc3645bc15f4
Hoàng Hồng Khang <hoanghongkhang@gmail.com>
2019-03-09 09:01:33 +0700

    lend and borrow book

---
+++ list_lend_book.php
@@ -0,0 +1,36 @@
+<div class="pb-3">
+    <h3>Sách đã đăng: </h3>
+    <div class="card mr-3" style="width: 15rem; float: left">
+        <img class="card-img-top" src="https://via.placeholder.com/150x150" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">ten sach</h5>
+            <p class="card-text">ngay muon: 09/03/2019</p>
+            <a href="#" class="btn btn-primary">Xem thông tin</a>
+        </div>
+    </div>
+    <div class="card mr-3" style="width: 15em; float: left">
+        <img class="card-img-top" src="https://via.placeholder.com/150x150" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">ten sach</h5>
+            <p class="card-text">ngay muon: 09/03/2019</p>
+            <a href="#" class="btn btn-primary">Xem thông tin</a>
+        </div>
+    </div>
+    <div class="card mr-3" style="width: 15rem; float: left">
+        <img class="card-img-top" src="https://via.placeholder.com/150x150" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">ten sach</h5>
+            <p class="card-text">ngay muon: 09/03/2019</p>
+            <a href="#" class="btn btn-primary">Xem thông tin</a>
+        </div>
+    </div>
+    <div class="card mr-3" style="width: 15rem; float: left">
+        <img class="card-img-top" src="https://via.placeholder.com/150x150" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title">ten sach</h5>
+            <p class="card-text">ngay muon: 09/03/2019</p>
+            <a href="#" class="btn btn-primary">Xem thông tin</a>
+        </div>
+    </div>
+    <div style="clear: both"></div>
+</div>
