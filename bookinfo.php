c05809c0dca031aa066684b692fbdca0bc1c5ca9
Hoàng Hồng Khang <hoanghongkhang@gmail.com>
2019-03-09 14:34:12 +0700

    fix ui list book and book_info

--- bookinfo.php
+++ bookinfo.php
@@ -34,42 +34,214 @@ if(empty(isset($_GET['id']))){
 	<link rel="stylesheet prefetch" href="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css">
 	<script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.js"></script>
-	<title>book deliverer</title>
+    <title>book deliverer</title>
+    <style>
+    
+/*****************globals*************/
+body {
+  overflow-x: hidden; }
+
+img {
+  max-width: 100%; }
+
+.preview {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+          flex-direction: column; }
+  @media screen and (max-width: 996px) {
+    .preview {
+      margin-bottom: 20px; } }
+
+.preview-pic {
+  -webkit-box-flex: 1;
+  -webkit-flex-grow: 1;
+      -ms-flex-positive: 1;
+          flex-grow: 1; }
+
+.preview-thumbnail.nav-tabs {
+  border: none;
+  margin-top: 15px; }
+  .preview-thumbnail.nav-tabs li {
+    width: 18%;
+    margin-right: 2.5%; }
+    .preview-thumbnail.nav-tabs li img {
+      max-width: 100%;
+      display: block; }
+    .preview-thumbnail.nav-tabs li a {
+      padding: 0;
+      margin: 0; }
+    .preview-thumbnail.nav-tabs li:last-of-type {
+      margin-right: 0; }
+
+.tab-content {
+  overflow: hidden; }
+  .tab-content img {
+    width: 100%;
+    -webkit-animation-name: opacity;
+            animation-name: opacity;
+    -webkit-animation-duration: .3s;
+            animation-duration: .3s; }
+
+.card {
+  margin-top: 50px;
+  background: #eee;
+  padding: 3em;
+  line-height: 1.5em; }
+
+@media screen and (min-width: 997px) {
+  .wrapper {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex; } }
+
+.details {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+          flex-direction: column; }
+
+.colors {
+  -webkit-box-flex: 1;
+  -webkit-flex-grow: 1;
+      -ms-flex-positive: 1;
+          flex-grow: 1; }
+
+.product-title, .price, .sizes, .colors {
+  text-transform: UPPERCASE;
+  font-weight: bold; }
+
+.checked, .price span {
+  color: #ff9f1a; }
+
+.product-title, .rating, .product-description, .price, .vote, .sizes {
+  margin-bottom: 15px; }
+
+.product-title {
+  margin-top: 0; }
+
+.size {
+  margin-right: 10px; }
+  .size:first-of-type {
+    margin-left: 40px; }
+
+.color {
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 10px;
+  height: 2em;
+  width: 2em;
+  border-radius: 2px; }
+  .color:first-of-type {
+    margin-left: 20px; }
+
+.add-to-cart, .like {
+  background: #ff9f1a;
+  padding: 1.2em 1.5em;
+  border: none;
+  text-transform: UPPERCASE;
+  font-weight: bold;
+  color: #fff;
+  -webkit-transition: background .3s ease;
+          transition: background .3s ease; }
+  .add-to-cart:hover, .like:hover {
+    background: #b36800;
+    color: #fff; }
+
+.not-available {
+  text-align: center;
+  line-height: 2em; }
+  .not-available:before {
+    font-family: fontawesome;
+    content: "\f00d";
+    color: #fff; }
+
+.orange {
+  background: #ff9f1a; }
+
+.green {
+  background: #85ad00; }
+
+.blue {
+  background: #0076ad; }
+
+.tooltip-inner {
+  padding: 1.3em; }
+
+@-webkit-keyframes opacity {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale(3);
+            transform: scale(3); }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale(1);
+            transform: scale(1); } }
+
+@keyframes opacity {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale(3);
+            transform: scale(3); }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale(1);
+            transform: scale(1); } }
+
+/*# sourceMappingURL=style.css.map */
+    </style>
 </head>
 <body>
 	<div class="container border">
 	<?php include 'header.php' ?>
 	<div class="container p-1">
-		<div class="row container">
-			<div class="col-3">
-				<img src="<?php echo $book['image'] ?>" alt="No image found">
-			</div>
-			<div class="row col-9">
-				<div class="col-6 container">
-					<span class="h1"><?php echo $book['name']; ?></span>
-					<div><?php echo $book['rating']; ?>/10</div>
-					<div class="h5"><?php echo $book['author']; ?></div>
-				</div>
-				<div class="col-6 container">
-					<div>Category
+        <h3>Book info</h3>
+    <div class="card mt-0 p-4">
+				<div class="wrapper row">
+					<div class="preview col-md-6">
+						
+						<div class="preview-pic tab-content">
+						  <div class="tab-pane active" id="pic-1"><img height="700px" src="<?php echo $book['image'] ?>" /></div>
+						</div>
+					</div>
+					<div class="details col-md-6">
+						<h3 class="product-title mb-0"><?php echo $book["name"] ?></h3>
+                        <h5 class="mb-2">Tác giả: <?php echo $book['author']; ?></h5>
+                        <div class="mb-2">Người đăng:<a href="./userprofile.php?id=<?php echo $author['id']?>"> <?php echo $author['fullname']; ?> </a></div>
+						<div class="rating">
+							<div class="stars">
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star"></span>
+								<span class="fa fa-star"></span>
+							</div>
+							<span class="review-no">41 reviews</span>
+						</div>
+                        <p class="product-description"><?php echo $book["description"] ?></p>
+                        <div>
+                            <h5>Category</h5>
 						<ul>
 							<li>Trinh thám</li>
 							<li>Tiểu thuyết</li>
 						</ul></div>
-						<div>Người đăng:<a href="./userprofile.php?id=<?php echo $author['id']?>"> <?php echo $author['fullname']; ?> </a></div>
-					</div>
-					<div class="row">
-						<button>Mượn</button>
-						<button>Thêm vào giỏ</button>
+						<div class="action">
+							<button class="add-to-cart btn btn-default" type="button">Mượn</button>
+						</div>
 					</div>
 				</div>
-			</div>
-			<!-- Decription -->
-			<div class="container">
-				<div class="h2">Description</div>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-
+		</div>
 			<!-- Comments -->
 			<div id="comments" class="container">
 				<div class="h2">Comments</div>
--- books.php
+++ books.php
@@ -13,21 +13,19 @@
         $myrow = $obj->fetch_record("books");
         foreach ($myrow as $row) {
 ?>
-    <div class="row border mb-3 mx-2">
+    <div class="row border mb-3 mx-2 p-2">
         <img class="image col-3" src="<?php echo $row['image']; ?>">
         <div class="col-9">
-            <span class="h4"><a href="./bookinfo.php?id=<?php echo $row['id'] ?>"><?php  echo $row["name"];?> </a></span>
-            <span><?php $author = $obj->select_record('users',array("id"=>$row['user_id'],)) ;  echo $author['fullname'];?></span>
+            <div class="h4"><a href="./bookinfo.php?id=<?php echo $row['id'] ?>"><?php  echo $row["name"];?> </a></div>
+            <h5 class="mb-2">Tác giả: <?php echo $row['author']; ?></h5>
+            <div class="mb-2">Người đăng:<a href="./userprofile.php?id=<?php $author = $obj->select_record('users',array("id"=>$row['user_id'],));
+              echo $author['id']?>"> <?php echo $author['fullname']; ?> </a></div>
             <span class="bg-success"><?php $row['status']==1?'Avaiable':'Borrowed'?></span>
             <div class="row">
-                <p class="col-10"> Abstraction: <?php echo $row['description']; ?></p>
+                <p class="col-10">Mô tả: <?php echo $row['description']; ?></p>
                 <div class="col-2">
                     <button class="btn btn-default">Borrow</button>
                 </div>
-            </div>
-            <div class="row">
-                <span class="col-6">Name of Provider</span>
-                <span class="col-6"><?php echo $row['rating'] ?>/10</span>
             </div>
         </div>
     </div>
